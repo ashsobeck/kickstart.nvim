9daf02acd7d80625abaa02a79664432efca154fd
Ashton Sobeck <asobeck99@gmail.com>
2023-10-22 22:04:49 -0400

    feat(plugins): custom plugins init

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    {
+        'theprimeagen/harpoon',
+        config = function()
+            local mark = require('harpoon.mark')
+            local ui = require('harpoon.ui')
+
+            vim.keymap.set('n', '<leader>a', mark.add_file)
+            vim.keymap.set('n', '<C-e', ui.toggle_quick_menu)
+
+            vim.keymap.set('n', '<C-h', function() ui.nav_file(1) end)
+            vim.keymap.set('n', '<C-g', function() ui.nav_file(2) end)
+            vim.keymap.set('n', '<C-,', function() ui.nav_file(3) end)
+            vim.keymap.set('n', '<C-.', function() ui.nav_file(4) end)
+        end
+    }
+}
